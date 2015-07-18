322b8ffda690b37f923288dabd8b36cd77de4409
AdinSk <andrej14046@gmail.com>
2015-07-18 13:40:09 +0200

    Create PhaseChangeEvent.java

---
+++ sk/Adin/Anni/api/PhaseChangeEvent.java
@@ -0,0 +1,28 @@
+package sk.Adin.Anni.api;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PhaseChangeEvent extends Event {
+
+    private int p;
+    
+    public PhaseChangeEvent(int p) {
+        this.p = p;
+    }
+    
+    private static final HandlerList handlers = new HandlerList();
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public int getNewPhase() {
+        return p;
+    }
+}
